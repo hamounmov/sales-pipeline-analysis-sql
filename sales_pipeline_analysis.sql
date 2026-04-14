48a963ec6d5621173f1244c76675439e15a616a1
hamounmov <hamounmovassagian@googlemail.com>
2026-04-14 17:01:37 +0100

    Update sales_pipeline_analysis.sql

--- sales_pipeline_analysis.sql
+++ sales_pipeline_analysis.sql
@@ -1,1 +1,10 @@
+/*
+Sales Pipeline Analysis
+Objective: identify what drives uneven pipeline conversion and revenue predictability
+*/
 
+--------------------------------------------------
+-- 1. Baseline metrics
+-- Overall closed win rate, average deal value, average time to close
+--------------------------------------------------
+  
